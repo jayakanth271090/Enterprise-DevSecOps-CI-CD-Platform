22d54140a4ec0e0760cf50ef80dbfcf1b36a50a0
jayakanth2710 <jayakanth2710@gmail.com>
2026-08-17 11:47:44 +0530

    Add Docker and Jenkins pipeline configuration

---
+++ app/devsecops-app/src/main/java/com/devsecops/devsecops_app/DevsecopsAppApplication.java
@@ -0,0 +1,13 @@
+package com.devsecops.devsecops_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsecopsAppApplication.class, args);
+	}
+
+}
